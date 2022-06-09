4c8506009e141dd0da2ac813b4f2f9040671fb3b
Aucke Bos <aucke.bos97@gmail.com>
2022-06-09 21:53:44 +0200

    WIP: Autodeploy

--- helpers.py
+++ helpers.py
@@ -1,7 +1,31 @@
+import datetime
 import json
 import os
+import requests
 
-from setup import CONFIG_DIR, CONFIG_FILE
+CONFIG_DIR = os.path.dirname(__file__) + "/config"
+CONFIG_FILE = f"{CONFIG_DIR}/cfg.json"
+
+DEPLOYMENT_TYPES = {
+    1: "Simple run `git pull repo_url` in a specific directory",
+    2: "Run a custom bash script when the webhook is called",
+    3: "Run a custom python function when the webhook is called",
+    4: "1 and 2",
+    5: "1 and 3",
+}
+
+
+def log(msg, error=False, fatal=False):
+    """
+    Helper function to log any data to stdout
+    """
+    typestring = "E" if error else "I"
+    print(
+        f'[{typestring}] [{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] - '
+        f"{msg}"
+    )
+    if fatal:
+        exit()
 
 
 def setup_needed() -> bool:
@@ -17,11 +41,34 @@ def get_config(key=None):
     complete dict
     """
     if setup_needed():
-        print("Please run the setup script: python setup.py")
-        exit()
+        log("Please run the setup script: python setup.py", True, True)
 
     with open(CONFIG_FILE) as file:
         cfg = json.load(file)
         if key is not None:
             return cfg[key]
         return cfg
+
+
+def get_public_ip():
+    """
+    Get the current public ip address
+    """
+    return requests.get("http://ipinfo.io/json").json()["ip"]
+
+
+def process_deployment_type(_type: int):
+    """
+    Parse the deployment type
+    :param type:  The type
+    :return: Tuple[bool,bool,bool]
+    Indiciating whether to:
+        - Do a simple git pull
+        - Run the bash script
+        - Run the python function
+    """
+    do_simple_pull = _type in (1, 4, 5)
+    run_bash_script = _type in (2, 5)
+    run_python_function = _type in (3, 5)
+
+    return do_simple_pull, run_bash_script, run_python_function
--- setup.py
+++ setup.py
@@ -1,14 +1,65 @@
 import os
 import json
+import secrets
 
-CONFIG_DIR = os.path.dirname(__file__) + "/config"
-CONFIG_FILE = f"{CONFIG_DIR}/cfg.json"
+from helpers import (
+    log,
+    get_public_ip,
+    CONFIG_DIR,
+    CONFIG_FILE,
+    get_config,
+    DEPLOYMENT_TYPES,
+    process_deployment_type,
+)
 
 
 def setup():
     if os.path.exists(CONFIG_DIR):
-        print("Config folder yet exists. To re-run the setup, delete your config dir")
-        exit()
+        log(
+            "Config folder yet exists. To re-run the setup, delete your config dir",
+            True,
+            True,
+        )
+    setup_server()
+    setup_deployment_type()
+    log("Config saved")
+
+
+def setup_deployment_type():
+    print(
+        "There are 5 different methods for deployment. The chosen method describes "
+        "what is to be done when a call to the webhook is received. The methods are as "
+        "follows:\n"
+    )
+    text = "\n".join([f"{key}: {value}" for key, value in DEPLOYMENT_TYPES.items()])
+    print(text + "\n")
+    _type = int(input("Please select a type, by entering its number: "))
+    do_simple_pull, run_bash_script, run_python_function = process_deployment_type(
+        _type
+    )
+
+    cfg = get_config()
+    if do_simple_pull:
+        cfg["repo_location"] = input(
+            "Please provide the absolute folder path in which you want to pull. "
+            "Important: The directory must already be a git repo, eg it must already "
+            "be clonsed once: "
+        )
+    if run_bash_script:
+        print(
+            "Create your bash script, save it as `deploy.sh` in the root of this "
+            "repository. It will be called when the webhook is called by Github"
+        )
+    if run_python_function:
+        print(
+            "Implement your python function in the function `custom_deploy` in `app.py`"
+        )
+    cfg["deployment_type"] = _type
+    with open(CONFIG_FILE, "w") as file:
+        json.dump(cfg, file)
+
+
+def setup_server():
     print(
         "Important: To receive webhook calls from github, a Flask server will be "
         "running permanently on your machine. This server needs to be publically "
@@ -20,10 +71,19 @@ def setup():
     cfg = {
         "port": input("On which port should the server listen? [default: 8855]:")
         or 8855,
+        "secret": secrets.token_hex(16),
     }
     os.mkdir(CONFIG_DIR)
-    with open(CONFIG_FILE, "x") as file:
+    with open(CONFIG_FILE, "w") as file:
         json.dump(cfg, file)
+    ip = get_public_ip()
+    port = cfg["port"]
+    secret = cfg["secret"]
+    url = f"http://{ip}:{port}/deploy"
+    print(
+        f"\nUse the following info for your Github Webhook:\n Payload URL: {url}\n "
+        f"Secret: {secret}\n"
+    )
 
 
 if __name__ == "__main__":
